1547f0eae3960a83464a600d8c64c22b4706fb1e
Ilya Khaprov <dead.trickster@gmail.com>
2015-11-24 01:38:57 +0300

    value-timestamp

--- src/grovel.lisp
+++ src/grovel.lisp
@@ -249,6 +249,7 @@
          (value-u64 "value.u64" :type uint64-t)
          (value-f32 "value.f32" :type :float)
          (value-f64 "value.f64" :type :double)
+         (value-timestamp "value.u64" :type uint64-t)
          (value-decimal "value.decimal" :type (:struct amqp-decimal-t))
          (value-bytes "value.bytes" :type (:struct amqp-bytes-t))
          (value-table "value.table" :type (:struct amqp-table-t))
